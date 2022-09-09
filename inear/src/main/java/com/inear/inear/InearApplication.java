f96de89fc5cd607ea015221b04682369dfa28966
judy5050 <qkrgywjd456@naver.com>
2022-09-09 17:27:33 +0900

    :hammer: #2 init: 프로젝트 초기 세팅

---
+++ inear/src/main/java/com/inear/inear/InearApplication.java
@@ -0,0 +1,13 @@
+package com.inear.inear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InearApplication.class, args);
+	}
+
+}
